38c26a61653b136f56f7331bf84d4f416f798876
Sysprogs <sysprogs@sysprogs.com>
2021-03-01 16:03:31 -0800

    Restructured the i.MXRT1050 FLASH programming example to implement the FLASH plugin API

--- imxrt/IMXRT1050_HyperFLASH_ROMAPI/main.cpp
+++ imxrt/IMXRT1050_HyperFLASH_ROMAPI/main.cpp
@@ -18,30 +18,21 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-#define BUFFER_LEN FLASH_PAGE_SIZE
-
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
 /* Get FlexSPI NOR Configuration Block */
 void FLEXSPI_NorFlash_GetConfig(flexspi_nor_config_t *config);
 void error_trap(void);
-void app_finalize(void);
 
-#if !defined(XIP_EXTERNAL_FLASH) || (XIP_EXTERNAL_FLASH != 1)
-extern status_t FLEXSPI_NorFlash_VerifyID(uint32_t instance);
-#endif
-
+#pragma region Functions from the SDK
 /*******************************************************************************
  * Variables
  ******************************************************************************/
 
 /*! @brief FLEXSPI NOR flash driver Structure */
 static flexspi_nor_config_t norConfig;
-/*! @brief Buffer for program */
-static uint8_t s_buffer[BUFFER_LEN];
-/*! @brief Buffer for readback */
-static uint8_t s_buffer_rbc[BUFFER_LEN];
+
 
 /*******************************************************************************
  * Code
@@ -225,154 +216,86 @@ void FLEXSPI_NorFlash_GetConfig(flexspi_nor_config_t *config)
     config->memConfig.lutCustomSeq[NOR_CMD_INDEX_CHIPERASE].seqId    = NOR_CMD_LUT_SEQ_IDX_CHIPERASE;
 }
 
-
-/*
- * @brief Gets called when an error occurs.
- *
- * @details Print error message and trap forever.
- */
 void error_trap(void)
 {
-    PRINTF("\r\n\r\n\r\n\t---- HALTED DUE TO FLEXSPI NOR ERROR! ----");
-    while (1)
-    {
-    }
+	asm("bkpt 255");
 }
 
-/*
- * @brief Gets called when the app is complete.
- *
- * @details Print finshed message and trap forever.
- */
-void app_finalize(void)
+#pragma endregion
+
+FLASHBankInfo FLASHPlugin_Probe(unsigned base, unsigned size, unsigned width1, unsigned width2)
 {
-    /* Print finished message. */
-    PRINTF("\r\n End of FLEXSPI NOR Example! \r\n");
-    while (1)
-    {
-    }
+	FLASHBankInfo result = {
+		.BaseAddress = base, 
+		.BlockCount = norConfig.memConfig.sflashA1Size /  norConfig.sectorSize,
+		.BlockSize = norConfig.sectorSize,
+		.WriteBlockSize = MINIMUM_PROGRAMMED_BLOCK_SIZE
+	};
+	
+	return result;
+}
+
+WorkAreaInfo FLASHPlugin_FindWorkArea(void *endOfStack)
+{
+	InterruptEnabler enabler;
+    
+	WorkAreaInfo info = { .Address = endOfStack, .Size = 4096 };
+	return info;
+}
+
+int FLASHPlugin_EraseSectors(unsigned firstSector, unsigned sectorCount)
+{
+	status_t status = ROM_FLEXSPI_NorFlash_Erase(FlexSpiInstance, &norConfig, firstSector * norConfig.sectorSize, sectorCount * norConfig.sectorSize);
+	if (status != kStatus_Success)
+		error_trap();
+
+	return sectorCount;
+}
+
+int FLASHPlugin_Unload()
+{
+	return 0;
+}
+
+int FLASHPlugin_DoProgramSync(unsigned startOffset, const void *pData, int bytesToWrite)
+{
+	int sectors = bytesToWrite / FLASH_PAGE_SIZE;
+	for (int i = 0; i < sectors; i++)
+	{
+		status_t status = ROM_FLEXSPI_NorFlash_ProgramPage(FlexSpiInstance,
+			&norConfig,
+			startOffset + i * FLASH_PAGE_SIZE,
+			(const uint32_t *)((const char *)pData + i  * FLASH_PAGE_SIZE));
+		
+		if (status != kStatus_Success)
+			return i * FLASH_PAGE_SIZE;
+	}
+	
+	return sectors * FLASH_PAGE_SIZE;
 }
 
 int main(void)
 {
-    status_t status;
-    uint32_t i = 0U;
-    uint32_t serialNorAddress;        /* Address of the serial nor device location */
-    uint32_t FlexSPISerialNorAddress; /* Address of the serial nor device in FLEXSPI memory */
-    uint32_t serialNorTotalSize;
-    uint32_t serialNorSectorSize;
-    uint32_t serialNorPageSize;
-
     //BOARD_ConfigMPU();
     BOARD_InitPins();
     //BOARD_BootClockRUN();
+	
+	memset(&norConfig, 0, sizeof(flexspi_nor_config_t));
 
-    PRINTF("\r\n FLEXSPI NOR example started!\r\n");
-    /* Clean up FLEXSPI NOR flash driver Structure */
-    memset(&norConfig, 0, sizeof(flexspi_nor_config_t));
+	FLEXSPI_NorFlash_GetConfig(&norConfig);
+	status_t status = ROM_FLEXSPI_NorFlash_Init(FlexSpiInstance, &norConfig);
+	if (status != kStatus_Success)
+		error_trap();
 
-    /* Setup FLEXSPI NOR Configuration Block */
-    FLEXSPI_NorFlash_GetConfig(&norConfig);
-
-    /* Initializes the FLEXSPI module for the other FLEXSPI APIs */
-    status = ROM_FLEXSPI_NorFlash_Init(FlexSpiInstance, &norConfig);
-    if (status == kStatus_Success)
-    {
-        PRINTF("\r\n Successfully init FLEXSPI serial NOR flash\r\n ");
-    }
-    else
-    {
-        PRINTF("\r\n Erase sector failure !\r\n");
-        error_trap();
-    }
-
-    /* Perform software reset after initializing flexspi module */
-    ROM_FLEXSPI_NorFlash_ClearCache(FlexSpiInstance);
-
-#if !defined(XIP_EXTERNAL_FLASH) || (XIP_EXTERNAL_FLASH != 1)
-    /* Read ID-CFI Parameters */
-    status = FLEXSPI_NorFlash_VerifyID(FlexSpiInstance);
-    if (status == kStatus_Success)
-    {
-        PRINTF("\r\n HyperFlash has been found successfully\r\n ");
-    }
-    else
-    {
-        PRINTF("\r\n HyperFlash can not be found!\r\n");
-        error_trap();
-    }
-#endif // XIP_EXTERNAL_FLASH
-
-    serialNorTotalSize  = norConfig.memConfig.sflashA1Size;
-    serialNorSectorSize = norConfig.sectorSize;
-    serialNorPageSize   = norConfig.pageSize;
-
-    /* Print HyperFlash information */
-    PRINTF("\r\n HyperFlash Information: ");
-    PRINTF("\r\n Total program flash size:\t%d KB, Hex: (0x%x)", (serialNorTotalSize / 1024U), serialNorTotalSize);
-    PRINTF("\r\n Program flash sector size:\t%d KB, Hex: (0x%x) ", (serialNorSectorSize / 1024U), serialNorSectorSize);
-    PRINTF("\r\n Program flash page size:\t%d B, Hex: (0x%x)\r\n", serialNorPageSize, serialNorPageSize);
-
-/*
- * SECTOR_INDEX_FROM_END = 1 means the last sector,
- * SECTOR_INDEX_FROM_END = 2 means (the last sector - 1) ...
- */
-#ifndef SECTOR_INDEX_FROM_END
-#define SECTOR_INDEX_FROM_END 1U
+	/* Perform software reset after initializing flexspi module */
+	ROM_FLEXSPI_NorFlash_ClearCache(FlexSpiInstance);
+	
+	FLASHPlugin_InitDone();
+    
+#ifdef DEBUG
+	TestFLASHProgramming(0x60000000, 0);
 #endif
-    /* Erase a sector from target device dest address */
-    serialNorAddress        = serialNorTotalSize - (SECTOR_INDEX_FROM_END * serialNorSectorSize);
-    FlexSPISerialNorAddress = EXAMPLE_FLEXSPI_AMBA_BASE + serialNorAddress;
 
-    /* Erase one sector. */
-    PRINTF("\r\n Erasing serial NOR flash over FLEXSPI");
-    status = ROM_FLEXSPI_NorFlash_Erase(FlexSpiInstance, &norConfig, serialNorAddress, serialNorSectorSize);
-    if (status == kStatus_Success)
-    {
-        /* Print message for user. */
-        PRINTF("\r\n Successfully erased one sector of NOR flash device 0x%x -> 0x%x\r\n", serialNorAddress,
-               (serialNorAddress + serialNorSectorSize));
-    }
-    else
-    {
-        PRINTF("\r\n Erase sector failure!\r\n");
-        error_trap();
-    }
-
-    PRINTF("\r\n Program a buffer to a page of NOR flash");
-    /* Prepare user buffer. */
-    for (i = 0; i < BUFFER_LEN; i++)
-    {
-        s_buffer[i] = i;
-    }
-
-    /* Program user buffer into FLEXSPI NOR flash */
-    status =
-        ROM_FLEXSPI_NorFlash_ProgramPage(FlexSpiInstance, &norConfig, serialNorAddress, (const uint32_t *)s_buffer);
-    if (status != kStatus_Success)
-    {
-        PRINTF("\r\n Page program failure!\r\n");
-        error_trap();
-    }
-
-    DCACHE_InvalidateByRange(FlexSPISerialNorAddress, sizeof(s_buffer_rbc));
-    /* Verify programming by reading back from FLEXSPI memory directly */
-    memcpy(s_buffer_rbc, (void *)(FlexSPISerialNorAddress), sizeof(s_buffer_rbc));
-    if (memcmp(s_buffer_rbc, s_buffer, sizeof(s_buffer)) == 0)
-    {
-        PRINTF("\r\n Successfully programmed and verified location FLEXSPI memory 0x%x -> 0x%x \r\n",
-               (FlexSPISerialNorAddress), (FlexSPISerialNorAddress + sizeof(s_buffer)));
-    }
-    else
-    {
-        PRINTF("\r\n Program data -  read out data value incorrect!\r\n ");
-        error_trap();
-    }
-
-    /* Erase the context we have progeammed before*/
-    status = ROM_FLEXSPI_NorFlash_Erase(FlexSpiInstance, &norConfig, serialNorAddress, serialNorSectorSize);
-
-    app_finalize();
 
     return 0;
 }
